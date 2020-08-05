3968f0f961900ba37e3ebe5149573871281df8b0
alienx456 <estebanelias27@gmail.com>
2020-08-05 00:51:43 -0500

    Tdd fase 1

--- queries.js
+++ queries.js
@@ -1,11 +1,47 @@
+var MongoClient = require('mongodb').MongoClient;
+
+const url = "mongodb://127.0.0.1:27017/"
+
+const mongdb = "salidas-db"
+
+const collection = "salidas" 
+
+
+
+
 const getSalidas = (request, response) => {
 
+
+    MongoClient.connect(url, function(err, db) {
+        if (err) throw err;
+        var dbo = db.db(mongdb);
+        dbo.collection(collection).find({}).toArray(function(err, result) {
+          if (err) throw err;
+          db.close();
+          response.status(200).json(result);
+        });
+      });
 
 }
 
 
 
 const postSalida = (request, response) => {
+
+    /*
+     * insertar correcciones y verificaciones
+     */
+
+    MongoClient.connect(url, function(err, db) {
+        if (err) throw err;
+        var dbo = db.db(mongdb);
+        var myobj = request.body;
+        dbo.collection(collection).insertOne(myobj, function(err, res) {
+          if (err) throw err;
+          db.close();
+          response.status(201).send("Carta de vuelo registrada")
+        });
+      });
     
 }
 
